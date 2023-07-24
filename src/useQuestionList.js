1de2f45515e632d85d1566f5b32d48062582b50b
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2023-07-24 21:16:35 +0900

    added q

--- src/useQuestionList.js
+++ src/useQuestionList.js
@@ -1,6 +1,8 @@
 import {
     useState
 } from 'react'
+import mosquitoVectors from './img/answer/mosquitoVectors.png';
+import filaria from './img/answer/filaria.png';
 
 // import from './img/answer/.png'
 
@@ -15,7 +17,7 @@ export const useQuestionList = () => {
                     detailInfo: '5/29',
                     questionImg: [],
                     questionSentence: 'ライム病について正しいのはどれか、3つ選べ',
-                    answerImg: [],
+                    answerImg: [mosquitoVectors],
                     choices: ["ダニが媒介する", "夏より冬に多く発症する", "病原体はリケッチアである", "皮膚紅斑がみられる", "テトラサイクリンが有効である"],
                     answer: 'ダニが媒介する・皮膚紅斑がみられる・テトラサイクリンが有効である。',
                     commentary: '',
@@ -93,7 +95,109 @@ export const useQuestionList = () => {
                 //     commentary: '',
                 //   },
             ],
-        },
+        }, {
+            groupTag: "2021本試験",
+            groupContents: [{
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '蚊媒介性ウイルス感染症でないものをすべて選べ。',
+                    choices: ["ジカ熱", "マラリア", "チワングニア熱", "フィラリア症", "日本脳炎"],
+                    answerImg: [mosquitoVectors, filaria],
+                    answer: 'マラリア・フィラリア',
+                    commentary: 'マラリア・フィラリアはウイルスではない。マラリアは原虫（細胞壁をもたず運動・捕食能力をもつ単細胞寄生虫）で、フィラリアは線虫（糸状の多細胞寄生虫）。選択肢の病原体はすべて蚊媒介性ではある。',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+                {
+                    detailInfo: '',
+                    questionImg: [],
+                    questionSentence: '',
+                    choices: ["", "", "", "", ""],
+                    answerImg: [],
+                    answer: '',
+                    commentary: '',
+                },
+            ]
+        }
     ])
     const showQuestionList = () => {
         return questionList
